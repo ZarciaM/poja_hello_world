7a4535adc10c97d8ffec5ad206afb6c36c3d0520
ZarciaM <154671918+ZarciaM@users.noreply.github.com>
2025-06-11 08:49:59 +0300

    Update HelloWorldController.java

--- src/main/java/com/example/demo/endpoint/rest/controller/health/HelloWorldController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/HelloWorldController.java
@@ -1,19 +1,23 @@
 package com.example.demo.endpoint.rest.controller.health;
 
-
-package com.example.demo.endpoint.rest.controller.health.HelloWorldService;
-import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@AllArgsConstructor
+
+
+@RestControllerMore actions
+
 public class HelloWorldController {
-  private final HelloWorldService service;
+
+
 
   @GetMapping("/hello")
-  public String helloWorld(@RequestParam String name) {
-    return service.uploadHelloWorldMessage(name);
+
+  public String helloWorld() {
+
+    return "... world successfully!";
+
   }
+
 }
