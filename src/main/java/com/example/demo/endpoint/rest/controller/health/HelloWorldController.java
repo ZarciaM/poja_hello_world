0e7ce219e293ce3aa022c4224c32d4e78ff121b4
ZarciaM <154671918+ZarciaM@users.noreply.github.com>
2025-06-12 08:09:49 +0300

    Update HelloWorldController.java

--- src/main/java/com/example/demo/endpoint/rest/controller/health/HelloWorldController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/HelloWorldController.java
@@ -5,12 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
 @RestControllerMore actions
 
 public class HelloWorldController {
-
-
 
   @GetMapping("/hello")
 
